89556240758adc51dc4bc9c3503e5516cf5062d2
ManishKonduri <manishkonduri008@gmail.com>
2022-11-23 16:59:27 +0530

    Adding 3 New Models

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,11 @@
 print("Adding all ML related code here")
+
+
+def model1():
+    print("Adding code for ML Model1")
+
+def model2():
+    print("Adding code for ML Model2")
+
+def model3():
+    print("Adding code for ML Model")
